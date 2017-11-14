37f98fd98668f6015743cbac07865bd8d19ff4bd
Simon Oulevay <simon.oulevay@gmail.com>
2017-11-14 14:55:50 +0100

    Allow to answer statements by clicking in the yes/no area in addition to dragging cards

--- src/app/question/question.component.ts
+++ src/app/question/question.component.ts
@@ -24,9 +24,13 @@ import { ChapterService } from '../_services/chapter.service';
 })
 
 export class QuestionComponent implements OnInit {
+
   @ViewChild('chapterInfoTab') chapterInfoTab: ElementRef;
-  @ViewChild('swSwing') swingStack: SwingStackComponent;
-  @ViewChildren('questionCard') swingCards: QueryList<SwingCardComponent>;
+  @ViewChild('yesArea') yesAreaElement: ElementRef;
+  @ViewChild('noArea') noAreaElement: ElementRef;
+  @ViewChild('swingViewport') viewportElement: ElementRef;
+  @ViewChild('swingStack') swingStack: SwingStackComponent;
+  @ViewChild('swingStack', { read: ElementRef }) stackElement: ElementRef;
 
   hammer: any;
   stackConfig: StackConfig;
@@ -36,7 +40,6 @@ export class QuestionComponent implements OnInit {
   refuse: boolean;
 
   statements: Statement[];
-  currentCardId: number;
   currentCardIndex: number;
   chapter: Chapter;
   opacity: number = 0;
@@ -44,6 +47,9 @@ export class QuestionComponent implements OnInit {
   opacityRight: number = 0;
   showHelp = true;
   tabVisible = false;
+  selectedAnswer: string;
+
+  private viewportClick: MouseEvent;
 
   constructor(
     private _tinderService: TinderService,
@@ -52,8 +58,7 @@ export class QuestionComponent implements OnInit {
   ) {
 
     this.stackConfig = {
-      // Default setting only allows UP, LEFT and RIGHT so you can override this as below
-      allowedDirections: [/*Direction.UP, Direction.DOWN, */Direction.LEFT, Direction.RIGHT],
+      allowedDirections: [ Direction.LEFT, Direction.RIGHT ],
       // Now need to send offsetX and offsetY with element instead of just offset
       throwOutConfidence: (offsetX, offsetY, element) => {
         return Math.min(
@@ -74,7 +79,6 @@ export class QuestionComponent implements OnInit {
   }
 
   ngOnInit() {
-    // Get statements from current chapter Observable
 
     if (!localStorage.getItem('hideHelp')) {
       this.showHelp = true;
@@ -82,10 +86,17 @@ export class QuestionComponent implements OnInit {
       this.showHelp = false;
     }
 
-
+    // Get statements from current chapter Observable
     this._chapterService.currentChapter.subscribe(res => {
+
       this.chapter = res;
-      this.statements = res.Statements;
+
+      if (res.Statements) {
+        // Reverse the statements because the cards appear in reverse order
+        this.statements = res.Statements.reverse();
+        this.currentCardIndex = this.statements.length - 1;
+      }
+
       if (!res || !res.Statements || res.Statements.length === 0) {
         this._router.navigate(['situation']);
       }
@@ -121,22 +132,54 @@ export class QuestionComponent implements OnInit {
     this.hammer.on('swipe', event => {
       event.preventDefault();
       if (event.deltaY > 0 && !this.tabVisible) {
+        // Swipe down (deltaY positive)
         this.tabVisible = true;
       } else if (event.deltaY < 0 && this.tabVisible) {
+        // Swipe up (deltaY negative)
         this.tabVisible = false;
       }
+    });
+
+    // Answer yes when clicking in the yes area
+    this.yesAreaElement.nativeElement.addEventListener('click', event => {
+      this.answerCurrentCard(true);
+    });
+
+    // Answer yes when clicking in the no area
+    this.noAreaElement.nativeElement.addEventListener('click', event => {
+      this.answerCurrentCard(false);
+    });
+
+    // Answer when clicking in the swing viewport in the yes or no area.
+    //
+    // In order to listen for click events in the swing viewport, we must first
+    // keep track of mousedown events. This is because sometimes a click event is
+    // triggered when it was in fact a drag of a card.
+    //
+    // We therefore keep the mousedown event in a variable, so that when we get
+    // the mouseup event later, we can know whether it was a click (same event
+    // coordinates) or a drag (differente event coordinates).
+    this.viewportElement.nativeElement.addEventListener('mousedown', event => {
+      this.viewportClick = event;
+    });
+
+    // Answer when clicking in the swing viewport in the yes or no area
+    this.viewportElement.nativeElement.addEventListener('mouseup', event => {
+      if (!this.viewportClick || event.x !== this.viewportClick.x || event.y !== this.viewportClick.y || this.isWithinElement(this.stackElement.nativeElement.getBoundingClientRect(), event)) {
+        // Ignore drag events (coordinates have changed since the last mousedown event)
+        // and clicks in the card area
+      } else if (this.isWithinElement(this.yesAreaElement.nativeElement.getBoundingClientRect(), event)) {
+        this.answerCurrentCard(true);
+      } else if (this.isWithinElement(this.noAreaElement.nativeElement.getBoundingClientRect(), event)) {
+        this.answerCurrentCard(false);
+      }
+
+      this.viewportClick = undefined;
     });
 
     this.swingStack.throwin.subscribe((event: ThrowEvent) => {
       this.opacityLeft = 0;
       this.opacityRight = 0;
-    });
-
-    this.swingStack.dragstart.subscribe((event: DragEvent) => {
-
-      this.currentCardId = parseInt(event.target.id.split('-')[1], 10);
-      this.currentCardIndex = parseInt(event.target.id.split('-')[2], 10);
-
     });
 
     this.swingStack.dragend.subscribe((event: DragEvent) => {
@@ -150,19 +193,19 @@ export class QuestionComponent implements OnInit {
       //this.opacity = this.confidenceCalculator(event.throwOutConfidence, 30) * 0.7;
 
       //if (this.opacityLeft != this.opacity && this.opacityRight != this.opacity) {
-      if (event.throwDirection.toString() === 'Symbol(RIGHT)' && event.throwOutConfidence === 1) {
+      if (event.throwDirection === Direction.RIGHT && event.throwOutConfidence === 1) {
         //this.opacityRight = Math.round(event.throwOutConfidence * precision) / precision * 0.7;
         this.opacityRight = 0.5
         this.opacityLeft = 0;
-        // Add the classes only to the current card (element in list that is the currentCardId)
+        // Add the classes only to the current card (element in list that is the currentCardIndex)
         this.statements[this.currentCardIndex].accepting = false;
         this.statements[this.currentCardIndex].refusing = true;
 
-      } else if (event.throwDirection.toString() === 'Symbol(LEFT)' && event.throwOutConfidence === 1) {
+      } else if (event.throwDirection === Direction.LEFT && event.throwOutConfidence === 1) {
         //this.opacityLeft = Math.round(event.throwOutConfidence * precision) / precision * 0.7;
         this.opacityLeft = 0.5;
         this.opacityRight = 0;
-        // Add the classes only to the current card (element in list that is the currentCardId)
+        // Add the classes only to the current card (element in list that is the currentCardIndex)
         this.statements[this.currentCardIndex].accepting = true;
         this.statements[this.currentCardIndex].refusing = false;
 
@@ -196,22 +239,41 @@ export class QuestionComponent implements OnInit {
     /*if (navigator.vibrate) {
       navigator.vibrate(60);
     }*/
-    const id = event.target.attributes.getNamedItem('id').textContent;
-    const stid = id.split('-').length >= 2 ? parseInt(id.split('-')[1], 10) : 0;
-    const answer = event.throwDirection.toString() === 'Symbol(LEFT)';
+
+    this.currentCardIndex--;
+
+    const statementId = parseInt(event.target.getAttribute('card-statement'), 10);
+    const answer = event.throwDirection === Direction.LEFT;
     this.opacityLeft = 0.7;
     this.opacityRight = 0.7;
-    this._tinderService.postAnswer(answer, stid)
-      .subscribe(res => {
-        this.opacityLeft = 0;
-        this.opacityRight = 0;
-        this.counter++;
-        if (this.counter === this.statements.length) {
-          this._router.navigate(['/situation']);
-        }
-      });
+
+    this.answer(answer, isNaN(statementId) ? 0 : statementId);
   }
 
-  selectedAnswer: string;
+  answer(value: boolean, statementId: number) {
+    this._tinderService.postAnswer(value, statementId).subscribe(res => {
+      this.opacityLeft = 0;
+      this.opacityRight = 0;
+      this.counter++;
+      if (this.counter === this.statements.length) {
+        this._router.navigate(['/situation']);
+      }
+    });
+  }
 
+  answerCurrentCard(value: boolean) {
+
+    const card = this.swingStack.cards[this.currentCardIndex];
+    const statementId = parseInt(card.getNativeElement().getAttribute('card-statement'), 10);
+
+    card.destroyCard();
+    card.getNativeElement().remove();
+    this.currentCardIndex--;
+
+    this.answer(value, isNaN(statementId) ? 0 : statementId);
+  }
+
+  isWithinElement(bounds: ClientRect, event: MouseEvent): boolean {
+    return event.x >= bounds.left && event.x <= bounds.left + bounds.width && event.y >= bounds.top && event.y <= bounds.top + bounds.height;
+  }
 }
