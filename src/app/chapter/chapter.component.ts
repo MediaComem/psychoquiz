babfbec35e2d27e9655a0d5d3e864b0bb7bab5d6
Simon Oulevay <simon.oulevay@gmail.com>
2017-12-04 10:03:19 +0100

    Send analytics event for profiles

--- src/app/chapter/chapter.component.ts
+++ src/app/chapter/chapter.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Angulartics2 } from 'angulartics2';
 import { ChapterService } from '../_services/chapter.service';
+import { ParticipationService } from '../_services/participation.service';
 import { TinderService } from '../_services/tinder.service';
 import { Chapter } from '../_models/chapter.model';
 import { Statement } from '../_models/statement.model';
@@ -17,12 +19,11 @@ export class ChapterComponent implements OnInit {
   loading: boolean;
 
   constructor(
+    private angulartics2: Angulartics2,
     private _chapterService: ChapterService,
+    private _participationService: ParticipationService,
     private _router: Router
   ) { }
-
-
-
 
   ngOnInit() {
     this.loading = true;
@@ -30,9 +31,8 @@ export class ChapterComponent implements OnInit {
     // problem: need to disable cache
     this._chapterService.getRandomChapter(this.token)
       .subscribe(res => {
-        
         if (res.finished) {
-          this._router.navigate(['/results']);
+          this.finish();
         } else {
           this.loading = false;
           this.chapter = res;
@@ -40,9 +40,33 @@ export class ChapterComponent implements OnInit {
 
       });
   }
-  
+
   start() {
     this._router.navigate(['situation/tdr']);
+  }
+
+  private finish() {
+    this._participationService.getResults().subscribe(results => {
+
+      let profile: any = {};
+
+      for (let i = 0; i < results.length; i++) {
+        const currentProfile = results[i];
+        if (profile.localPercent === undefined || currentProfile.localPercent >= profile.localPercent) {
+          profile = currentProfile;
+        }
+      }
+
+      this.angulartics2.eventTrack.next({
+        action: profile.name,
+        properties: {
+          category: 'Profil',
+          value: Math.round(profile.localPercent * 100)
+        }
+      });
+
+      this._router.navigate(['/results']);
+    });
   }
 
 }
